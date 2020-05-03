5fd8d40fbc8a8e6db77b4a84f4883daea2f5318f
yusrilwahyuda <43510262+yusrilwahyuda@users.noreply.github.com>
2020-05-03 17:56:39 +0800

    Update aws_textile.sql
    database baru

--- aws_textile.sql
+++ aws_textile.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 03 Bulan Mei 2020 pada 09.50
+-- Waktu pembuatan: 03 Bulan Mei 2020 pada 11.56
 -- Versi server: 10.1.31-MariaDB
 -- Versi PHP: 7.2.3
 
@@ -46,6 +46,30 @@ INSERT INTO `kehadiran` (`id_pegawai`, `nama`, `tanggal`, `jam_datang`, `jam_pul
 (9, 'Sherla Yualinda', '2020-05-02', '07:00', '15:00'),
 (11, 'Yusril Wahyuda', '2020-05-03', '07:00', '15:00');
 
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penjualan_sarung`
+--
+
+CREATE TABLE `penjualan_sarung` (
+  `id` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `sales` varchar(255) NOT NULL,
+  `no_Nota` varchar(255) NOT NULL,
+  `item` varchar(255) NOT NULL,
+  `qyt` varchar(255) NOT NULL,
+  `harga_perqyt` varchar(255) NOT NULL,
+  `keterangan` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `penjualan_sarung`
+--
+
+INSERT INTO `penjualan_sarung` (`id`, `tanggal`, `sales`, `no_Nota`, `item`, `qyt`, `harga_perqyt`, `keterangan`) VALUES
+(1, '2020-05-03', 'Facebook', '032/0918', 'Alisa', '5', '286000', 'BCA 2/9 2.350.000 JNE Tracking 100.000');
+
 --
 -- Indexes for dumped tables
 --
@@ -57,6 +81,12 @@ ALTER TABLE `kehadiran`
   ADD PRIMARY KEY (`id_pegawai`);
 
 --
+-- Indeks untuk tabel `penjualan_sarung`
+--
+ALTER TABLE `penjualan_sarung`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
@@ -65,6 +95,12 @@ ALTER TABLE `kehadiran`
 --
 ALTER TABLE `kehadiran`
   MODIFY `id_pegawai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT untuk tabel `penjualan_sarung`
+--
+ALTER TABLE `penjualan_sarung`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
